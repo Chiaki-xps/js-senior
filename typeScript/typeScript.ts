923ea90b21066f390b36f9e44fff97b6daa5c9e0
Chiaki-xps <1490295446@qq.com>
2024-04-30 22:41:33 +0800

    typescript

--- typeScript/typeScript.ts
+++ typeScript/typeScript.ts
@@ -1,1 +1,14 @@
+interface Person {
+  name: string;
+  age: number;
+}
 
+type K1 = keyof Person; // "name" | "age"
+type K2 = keyof Person[]; // "length" | "toString" | "pop" | "push" | "concat"...
+
+const k1: K1 = 'name';
+const k2: K2 = 'length';
+
+type K3 = keyof { [x: string]: Person };
+
+const k3: K3 = 123;
